6b3389a756e8ad268a1eccfd084c182a001a3d19
DavidLebrisse <david.lebrisse@etu.univ-cotedazur.fr>
2020-03-29 17:38:46 +0200

    Ajout Modification Titre/Theme/Image d'un quiz

--- front-end/src/app/quizzes/edit-quiz/edit-quiz.component.ts
+++ front-end/src/app/quizzes/edit-quiz/edit-quiz.component.ts
@@ -4,6 +4,7 @@ import { QuizService } from '../../../services/quiz.service';
 
 import { Quiz } from '../../../models/quiz.model';
 import { DomSanitizer } from '@angular/platform-browser';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -13,21 +14,70 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class EditQuizComponent implements OnInit {
   quizSelected: Quiz;
+  quizForm: FormGroup;
+  editionMode: Boolean = false;
 
-  constructor(private route: ActivatedRoute, public quizService: QuizService, private sanitizer: DomSanitizer) {
+  imgAsString: string;
+
+  constructor(private route: ActivatedRoute, public quizService: QuizService, private sanitizer: DomSanitizer, public formBuilder: FormBuilder) {
     
    }
 
   ngOnInit() {
-    this.quizService.quizSelected$.subscribe((quiz) => this.quizSelected = quiz);
+    this.quizService.quizSelected$.subscribe((quiz) => this.onQuizSelected(quiz));
     const id = this.route.snapshot.paramMap.get('id');
     this.quizService.setSelectedQuiz(id);
+  }
+
+  private onQuizSelected(quiz: Quiz) {
+    this.quizSelected = quiz;
+    this.initQuizForm();
+  }
+
+  private initQuizForm() {
+    if (this.quizSelected != null) {
+      this.quizForm = this.formBuilder.group({
+        name: this.quizSelected.name,
+        theme: this.quizSelected.theme
+      });
+    } else {
+      this.quizForm = this.formBuilder.group({
+        name: "",
+        theme: "Autres"
+      });
+    }
+  }
+
+  updateQuiz() {
+    let newNameAndTheme = this.quizForm.getRawValue() as {name: String, theme: String, image?: String};
+
+    if (this.quizSelected.image !== this.imgAsString) {
+      newNameAndTheme = {...newNameAndTheme, image: this.imgAsString};
+    }
+
+    this.quizService.updateQuiz(this.quizSelected,newNameAndTheme);
+    this.editionMode = false;
+  }
+
+  onChangeFile(event) {
+    let reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imgAsString = 'data:image;base64,' + (reader.result as string).split(',')[1];
+      };
+    }
   }
 
   sanitize(url: string) {
     if(!url) url = this.quizService.imageByDefault();
     return this.sanitizer.bypassSecurityTrustUrl(url);
   }
+
+
+
+
 
   /*getId(): void {
     const id = this.route.snapshot.paramMap.get('id');
