ebcd6f0da3642b5d0f4b8db23bf0e8b014c360c8
Angele Badia <angele.badia@etu.unice.fr>
2020-04-14 00:40:45 +0200

    affichage image question

--- front-end/src/app/quiz-sequence/question-widget/question-widget.component.ts
+++ front-end/src/app/quiz-sequence/question-widget/question-widget.component.ts
@@ -6,22 +6,25 @@ import { QuizService } from 'src/services/quiz.service';
 import { BehaviorSubject } from 'rxjs';
 import { AnswerListWidgetComponent } from '../answer-list-widget/answer-list-widget.component';
 import { Answer } from 'src/models/answer.model';
+import { Img } from '../../../models/image.model';
+import { ImageService } from 'src/services/image.service';
 
 @Component({
   selector: 'app-question-widget',
   templateUrl: './question-widget.component.html',
   styleUrls: ['./question-widget.component.scss']
 })
+
 export class QuestionWidgetComponent implements OnInit {
 
   quiz : Quiz;
   questions: Question[];
   currentQuestion : Question;
+  image: Img;
 
-  @Output()
-  showAnswer: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() showAnswer: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(private route: ActivatedRoute, public quizService: QuizService) {
+  constructor(private route: ActivatedRoute, public quizService: QuizService, public imageService: ImageService) {
     this.quizService.quizSelected$.subscribe((quiz) => {
       this.quiz = quiz;
       if(quiz.questions) {
@@ -41,7 +44,18 @@ export class QuestionWidgetComponent implements OnInit {
 
   onNext = () => this.changeQuestion();
 
-  changeQuestion = () => this.currentQuestion = this.questions.pop();
+  changeQuestion = () => {
+    this.currentQuestion = this.questions.pop();
+    this.loadImage();
+  }
+  
+  loadImage(){
+    this.image = {} as Img;
+    const id = this.currentQuestion.imageId;
+    if(id!=null) this.imageService.loadQuestionImage(this.image, id);
+  }
+
+  getImgSrc() { return this.imageService.sanitize(this.image.url) }
 }
 
 
