fa7e6f6956395d66c705db8d637154f9e2afbe7a
Angele Badia <angele.badia@etu.unice.fr>
2020-06-02 13:30:05 +0200

    favicon changed

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
-/*import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  // ...
-} from '@angular/animations';*/
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  /*animations: [
-    // animation triggers go here
-  ]*/
 })
 export class AppComponent {
   title = 'starter-quiz';
